14d4b3785f42056076eb7532aec1908db08b40f5
Ashot08 <albertguzeev@yandex.ru>
2023-08-05 13:38:50 +0700

    add profile page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,49 +1,7 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
 import path, { resolve } from 'path';
-
-const pageData = {
-    '/index.html': {
-        meta: {
-            title: 'Yandex Practicum Project',
-            description: 'Yandex Practicum middle frontend developer project by Ashot08',
-        },
-    },
-    '/pages/login/login.html': {
-        meta: {
-            title: 'Login Page',
-            description: 'Login Page - Yandex Practicum middle frontend developer project by Ashot08',
-        },
-    },
-    '/pages/404/404.html': {
-        meta: {
-            title: '404 - Not Found Page',
-            description: '404 Not Found Page - Yandex Practicum middle frontend developer project by Ashot08',
-        },
-        link: {
-            url: '/',
-            text: 'Назад к чатам'
-        },
-        error: {
-            code: '404',
-            description: 'Не туда попали'
-        }
-    },
-    '/pages/500/500.html': {
-        meta: {
-            title: '500 - Server Error',
-            description: '500 Server Error Page - Yandex Practicum middle frontend developer project by Ashot08',
-        },
-        link: {
-            url: '/',
-            text: 'Назад к чатам'
-        },
-        error: {
-            code: '500',
-            description: 'Мы уже фиксим'
-        }
-    }
-};
+import pageData from './src/pageData.json' assert {type: 'json'};
 
 export default defineConfig({
     root: resolve(__dirname, 'src'),
