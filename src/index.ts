fde03cbc6b16a32a267cf595182f6773d5b64263
Ashot08 <albertguzeev@yandex.ru>
2023-10-02 14:58:44 +0700

    add router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import registerComponent from './utils/registerComponent';
-import render from './utils/render';
 import Base from './components/layout/base';
 import Link from './components/link';
 import Page from './components/layout/page';
@@ -8,6 +7,14 @@ import Input from './components/input';
 import Button from './components/button';
 import Error from './components/error';
 import ProfileForm from './components/profile-form';
+import Router from './utils/Router';
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+import SignupPage from './pages/signup';
+import ProfilePage from './pages/profile';
+import ChatPage from './pages/chat';
+import Error404Page from './pages/404';
+import Error500Page from './pages/500';
 
 registerComponent('Input', Input);
 registerComponent('Button', Button);
@@ -17,7 +24,17 @@ registerComponent('Page', Page);
 registerComponent('AuthForm', AuthForm);
 registerComponent('Error', Error);
 registerComponent('ProfileForm', ProfileForm);
+registerComponent('HomePage', HomePage);
+registerComponent('LoginPage', LoginPage);
 
 window.addEventListener('DOMContentLoaded', () => {
-  render('home');
+  const router = new Router('#app');
+  router.use('/', HomePage);
+  router.use('/login', LoginPage);
+  router.use('/signup', SignupPage);
+  router.use('/profile', ProfilePage);
+  router.use('/chat', ChatPage);
+  router.use('/error404', Error404Page);
+  router.use('/error500', Error500Page);
+  router.start();
 });
---
+++ src/utils/Route.ts
@@ -0,0 +1,54 @@
+import isEqual from './isEqual';
+
+import Block from './Block';
+
+export default class Route {
+  private _pathname: string;
+
+  private _blockClass: typeof Block;
+
+  private _block: Block | null;
+
+  private _props: any;
+
+  constructor(pathname: string, view: typeof Block, props: object) {
+    this._pathname = pathname;
+    this._blockClass = view;
+    this._block = null;
+    this._props = props;
+  }
+
+  navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  leave() {
+    if (this._block) {
+      //this._block.hide();
+    }
+  }
+
+  match(pathname: string) {
+    return isEqual(pathname, this._pathname);
+  }
+
+  render() {
+    if (!this._block) {
+      this._block = new this._blockClass();
+    }
+
+    const root = document.querySelector(this._props.rootQuery);
+
+    if (!root) {
+      throw new Error('Root not found');
+    }
+
+    root.innerHTML = '';
+    if (this._block !== null) {
+      root.appendChild(this._block.getContent());
+    }
+  }
+}
